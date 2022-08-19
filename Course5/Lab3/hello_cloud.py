2f48d6f26d28420b154f1bd2756d77ce74793ac8
Anuradha Adhikari <106548408+Ramsey99@users.noreply.github.com>
2022-08-19 12:15:59 +0530

    Update hello_cloud.py

--- Course5/Lab3/hello_cloud.py
+++ Course5/Lab3/hello_cloud.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python3
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 """A simple Hello World type app which can serve on port 8000.
 Optionally, a different port can be passed.
 
-The code was inspired by:
-https://gist.github.com/davidbgk/b10113c3779b8388e96e6d0c44e03a74
 """
 import http
 import http.server
